c07f1fb023ef3d34cf8b3b937d35a5a9b8fb4788
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-18 19:57:38 +0700

    load sample data and run validation on all transaction, print result, validate 1 transaction

--- test.js
+++ test.js
@@ -4,3 +4,25 @@ import { validateTransaction } from "./src/main.js";
 import fs from "fs"; // file syetem module (built in Node.js)
 
 // ===== LOAD SAMPLE DATA ======
+// Read JSON file (sampleTransactions.json)
+// Parse JSON → JavaScript array
+const sampleData = JSON.parse(
+    fs.readFileSync("./data/sampleTransaction.json","utf-8")
+);
+
+// ======= RUN VALIDATION ON ALL TRANSACTION ==========
+console.log("TRANSACTION VALIDATION TEST\n");
+console.log("=".repeat(80));
+// Validate through every transaction
+sampleData.forEach((transaction) => {
+    // validate 1 transaction
+    const result = validateTransaction(transaction);
+
+    // print result
+    console.log(`\nTransaction ID ${result.transactionId}`);
+    console.log(`\nStatus: ${result.isValid ? "VALID" : "INVALID"}`);
+    console.log(`\nSumarry ${result.summary}`);
+}
+
+
+)
