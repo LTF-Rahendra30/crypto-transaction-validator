a4ee406bc7786560962ad4b4239b7970b489f1a4
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-18 12:29:29 +0700

    refactor: Add transactionId parameter in the method

--- src/validators/amountValidator.js
+++ src/validators/amountValidator.js
@@ -1,10 +1,24 @@
 import { ERROR_MASSAGE,createValidationResult } from "../utils/errors.js";
 
-export function validateAmount(amount){
+
+/**
+ * Validate transaction amount
+ * 
+ * Rules:
+ * 1. Must be a number
+ * 2. Must be positive (> 0)
+ * 3. Cannot be Infinity or NaN
+ * 
+ * Example valid: 1.5, 100, 0.001
+ * Example invalid: -5, 0, "100", Infinity
+ */
+
+export function validateAmount(amount,transactionId){
 
     if (typeof amount !== 'number'){
         return(
             false,
+            transactionId,
             `Amount must be a number, got ${typeof amount}`
         );
     }
