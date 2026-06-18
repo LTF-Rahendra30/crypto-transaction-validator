531b74329c0799ecb777456c87ee9243ea4d90a1
LTF-Rahendra30 <kecuu91@gmail.com>
2026-06-18 19:44:28 +0700

    feat: Import module from main.js to test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+// ====== IMPORT MODULES ======
+
+import { validateTransaction } from "./src/main.js";
+import fs from "fs"; // file syetem module (built in Node.js)
+
+// ===== LOAD SAMPLE DATA ======
